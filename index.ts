5fb24affb190bf1fd093d46643bc61db20219960
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-05-19 16:02:50 +0500

    update

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
 import inquirer from "inquirer"
+
+const answers: {
+    sentence: string
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word."
+    }
+])
+
+const words = answers.sentence.trim().split(" ")
+
+console.log(`Your sentence has ${words.length} words.`)
